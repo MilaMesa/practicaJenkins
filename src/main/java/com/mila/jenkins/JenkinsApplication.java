ceb2703fd0eae44371fe8a719859370d73ed96eb
MilaMesa <maria_caicedo01152@elpoli.edu.co>
2021-09-03 22:25:03 -0500

    camila
    se elimina codigo no usado, se aumenta covertura

--- src/main/java/com/mila/jenkins/JenkinsApplication.java
+++ src/main/java/com/mila/jenkins/JenkinsApplication.java
@@ -8,16 +8,6 @@ public class JenkinsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinsApplication.class, args);
-        System.out.println("test");
-        Calculadora calculadora = new Calculadora();
-        calculadora.setA(1);
-        calculadora.setB(5);
-        int sumado = calculadora.sumar();
-        int restado = calculadora.restar();
-        int multiplicado = calculadora.multiplicar();
-        System.out.println(sumado);
-        System.out.println(restado);
-        System.out.println(multiplicado);
     }
 
 }
